1df6b7f7b5df739a634a9b0d8916837293de539f
David Vujic <david@vujic.se>
2015-05-18 21:06:44 +0200

    Demologger comments
    The logger implementation is for demo purposes only

--- DevSum/BankWithEPiServer/DevBank.Logging/DemoLogger.cs
+++ DevSum/BankWithEPiServer/DevBank.Logging/DemoLogger.cs
@@ -4,6 +4,12 @@ namespace DevBank.Logging
 {
     public class DemoLogger : IBankLogger
     {
+		/// <summary>
+		/// This one is for demo and presentation purposes only. Don't use it at home, folks.
+		/// 
+		/// You probably would want to have an implementation that uses log4net.
+		/// </summary>
+		/// <param name="data">data to log</param>
 	    public void Add(object data)
 	    {
 			var serializer = new System.Xml.Serialization.XmlSerializer(data.GetType());
