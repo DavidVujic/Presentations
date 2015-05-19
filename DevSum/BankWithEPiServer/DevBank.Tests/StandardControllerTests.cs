b86f20b113273d328cf9d3e5d7d6062c8dc153f0
David Vujic <david@vujic.se>
2015-05-19 14:37:02 +0200

    Added example unit tests and FakeMaker

---
+++ DevSum/BankWithEPiServer/DevBank.Tests/StandardControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using DevBank.Business.ServiceFactories;
+using DevBank.Controllers;
+using DevBank.Models.Pages;
+using DevBank.Models.ViewModels;
+using Moq;
+using NUnit.Framework;
+
+namespace DevBank.Tests
+{
+	[TestFixture]
+	public class StandardControllerTests
+	{
+		[Test]
+		public void Should_return_view_model()
+		{
+			var factory = new Mock<ICustomerFactory>();
+
+			var page = new StandardPage();
+
+			var controller = new StandardController(factory.Object);
+
+			var result = controller.Index(page) as ViewResult;
+
+			Assert.IsNotNull(result);
+			Assert.True(result.Model is PageViewModel<StandardPage>);
+		}
+	}
+}
