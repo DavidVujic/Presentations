53b92b013f3d0133c6bf70f92c0ac6faf068fd15
David Vujic <david@vujic.se>
2015-05-25 23:06:43 +0200

    added a unit test proving missing functionality

--- DevSum/BankWithEPiServer/DevBank.Tests/SocialSecurityNumberValidatorTests.cs
+++ DevSum/BankWithEPiServer/DevBank.Tests/SocialSecurityNumberValidatorTests.cs
@@ -19,6 +19,24 @@ namespace DevBank.Tests
 			Assert.IsTrue(isValid);
 		}
 
+		/// <summary>
+		/// This unit test is an example of test driven development.
+		/// The validation attribute currently only sanitizes the input, but should also
+		/// (of course) do a full social security number checking! This unit test proves the missing functionality.
+		/// </summary>
+		[Test]
+		public void Should_not_accept_invalid_social_security_numbers()
+		{
+			// Arrange
+			var validator = new SocialSecurityNumberAttribute();
+
+			// Act
+			var isValid = validator.IsValid("221212121212");
+
+			// Assert
+			Assert.IsFalse(isValid);
+		}
+
 		[Test]
 		public void Should_not_accept_not_allowed_characters_in_social_security_numbers()
 		{
